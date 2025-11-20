d12b5e5ae86183514168255229d075dc3c980a7f
Reingold Shekhtel <raikbitters@gmail.com>
2025-11-20 18:00:53 +0100

    Remove unused HTTP router setup from main.go

--- cmd/ingest/main.go
+++ cmd/ingest/main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-)
-
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-	router.Use(middleware.Recoverer)
-	// router.Mount("/debug", middleware.Profiler())
 
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
-
-	router.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
-		panic("something went wrong")
-	})
-
-	http.ListenAndServe(":8080", router)
 }
