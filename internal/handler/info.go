a642e7957f821fc0c07bdd0d9f482004d7cc8a27
Reingold Shekhtel <raikbitters@gmail.com>
2025-11-28 19:03:47 +0100

    Add info handler with route for retrieving service information

---
+++ internal/handler/info.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func infoRouter() chi.Router {
+	r := chi.NewRouter()
+
+	r.Get("/info", getInfo)
+
+	return r
+}
+
+func getInfo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(`{"service":"Ingest Service","version":"1.0.0"}`))
+}
