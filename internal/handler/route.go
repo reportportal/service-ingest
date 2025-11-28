d63a21021bcc69ca241ba204729a7779520ebe3b
Reingold Shekhtel <raikbitters@gmail.com>
2025-11-28 16:41:33 +0100

    Refactor router setup to include API routes and improve structure

--- internal/handler/route.go
+++ internal/handler/route.go
@@ -5,13 +5,23 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func NewRouter(basePath string) *chi.Mux {
+func NewRouter(basePath string) chi.Router {
 	r := chi.NewRouter()
-
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Mount(basePath, health())
+	r.Mount(basePath, healthRouter())
+	r.Mount(basePath, apiRouter())
+
+	return r
+}
+
+func apiRouter() chi.Router {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+
+	r.Mount("/launch", launchHandler{}.routes())
+	r.Mount("/log", logHandler{}.routes())
 
 	return r
 }
