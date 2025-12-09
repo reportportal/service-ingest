17ed0235a2c4b5a0cecedccbaaf31deb3ac8cccd
Reingold Shekhtel <raikbitters@gmail.com>
2025-12-09 16:15:24 +0100

    Add item model with types, statuses, and request/response structures for test items

--- internal/handler/item_model.go
+++ internal/handler/item_model.go
@@ -1,6 +1,110 @@
 package handler
 
-import "github.com/reportportal/service-ingest/internal/model"
+import (
+	"net/http"
+	"time"
+
+	"github.com/reportportal/service-ingest/internal/model"
+)
+
+type StartTestItemRQ struct {
+	UUID        string         `json:"uuid" validate:"required,uuid"`
+	LaunchUUID  string         `json:"launchUuid" validate:"required,uuid"`
+	StartTime   time.Time      `json:"startTime" validate:"required,datetime"`
+	Name        string         `json:"name" validate:"required"`
+	Description string         `json:"description,omitempty"`
+	CodeRef     string         `json:"codeRef,omitempty"`
+	TestCaseId  string         `json:"testCaseId,omitempty"`
+	Attributes  Attributes     `json:"attributes,omitempty" validate:"max=256,dive,unique"`
+	Parameters  Parameters     `json:"parameters,omitempty"`
+	IsRetry     bool           `json:"retry,omitempty"`
+	HasStats    bool           `json:"hasStats,omitempty"`
+	RetryOf     string         `json:"retryOf,omitempty" validate:"omitempty,uuid"`
+	Type        model.ItemType `json:"type" validate:"required,oneof=SUITE STORY TEST SCENARIO STEP BEFORE_CLASS BEFORE_GROUPS BEFORE_METHOD BEFORE_SUITE BEFORE_TEST AFTER_CLASS AFTER_GROUPS AFTER_METHOD AFTER_SUITE AFTER_TEST"`
+}
+
+func (rq *StartTestItemRQ) Bind(_ *http.Request) error {
+	if err := validate.Struct(rq); err != nil {
+		return err
+	}
+
+	if rq.CodeRef == "" {
+		rq.CodeRef = rq.Name
+	}
+
+	if rq.TestCaseId == "" {
+		rq.TestCaseId = rq.CodeRef
+	}
+
+	return nil
+}
+
+func (rq *StartTestItemRQ) toItemModel() model.Item {
+	return model.Item{
+		UUID:        rq.UUID,
+		LaunchUUID:  rq.LaunchUUID,
+		Name:        rq.Name,
+		Description: rq.Description,
+		Type:        rq.Type,
+		StartTime:   rq.StartTime,
+		Attributes:  rq.Attributes.toAttributesModel(),
+		Parameters:  rq.Parameters.toParametersModel(),
+		CodeRef:     rq.CodeRef,
+		TestCaseId:  rq.TestCaseId,
+		IsRetry:     rq.IsRetry,
+		RetryOf:     rq.RetryOf,
+	}
+}
+
+type StartTestItemRS struct {
+	UUID string `json:"id"`
+}
+
+func (rs *StartTestItemRS) Render(_ http.ResponseWriter, _ *http.Request) error {
+	return nil
+}
+
+type FinishTestItemRQ struct {
+	LaunchUUID  string           `json:"launchUuid" validate:"required,uuid"`
+	EndTime     time.Time        `json:"endTime" validate:"required,datetime"`
+	Status      model.ItemStatus `json:"status,omitempty" validate:"omitempty,oneof=PASSED FAILED STOPPED SKIPPED INTERRUPTED CANCELLED INFO WARN"`
+	Attributes  Attributes       `json:"attributes,omitempty" validate:"max=256,dive,unique"`
+	Description string           `json:"description,omitempty"`
+	TestCaseId  string           `json:"testCaseId,omitempty"`
+	IsRetry     bool             `json:"retry,omitempty"`
+	RetryOf     string           `json:"retryOf,omitempty" validate:"omitempty,uuid"`
+	Issue       Issue            `json:"issue,omitempty"`
+}
+
+func (rq *FinishTestItemRQ) Bind(_ *http.Request) error {
+	if err := validate.Struct(rq); err != nil {
+		return err
+	}
+
+	if rq.Status == "" {
+		rq.Status = model.ItemStatusPassed
+	}
+	return nil
+}
+
+func (rq *FinishTestItemRQ) toItemModel() model.Item {
+	return model.Item{
+		LaunchUUID:  rq.LaunchUUID,
+		EndTime:     rq.EndTime,
+		Status:      rq.Status,
+		Attributes:  rq.Attributes.toAttributesModel(),
+		Description: rq.Description,
+		TestCaseId:  rq.TestCaseId,
+	}
+}
+
+type FinishTestItemRS struct {
+	Message string `json:"message"`
+}
+
+func (rs *FinishTestItemRS) Render(_ http.ResponseWriter, _ *http.Request) error {
+	return nil
+}
 
 type ItemAttribute struct {
 	Key      string `json:"key,omitempty"`
@@ -11,17 +115,85 @@ type ItemAttribute struct {
 type Attributes []ItemAttribute
 
 func (a Attributes) toAttributesModel() model.Attributes {
-	attrs := make([]model.Attribute, 0, len(a))
-	for _, a := range a {
-		attrs = append(attrs, model.Attribute(a))
+	attrs := make(model.Attributes, len(a))
+	for i, attr := range a {
+		attrs[i] = model.Attribute(attr)
 	}
 	return attrs
 }
 
-func fromAttributesModel(modelAttrs model.Attributes) Attributes {
-	attrs := make(Attributes, len(modelAttrs))
-	for i, attr := range modelAttrs {
+func fromAttributesModel(model model.Attributes) Attributes {
+	attrs := make(Attributes, len(model))
+	for i, attr := range model {
 		attrs[i] = ItemAttribute(attr)
 	}
 	return attrs
+}
+
+type Parameter struct {
+	Key   string `json:"key" validate:"required"`
+	Value string `json:"value,omitempty"`
+}
+
+type Parameters []Parameter
+
+func (p Parameters) toParametersModel() model.Parameters {
+	params := make(model.Parameters, len(p))
+	for i, param := range p {
+		params[i] = model.Parameter(param)
+	}
+	return params
+}
+
+func fromParametersModel(model model.Parameters) Parameters {
+	params := make(Parameters, len(model))
+	for i, param := range model {
+		params[i] = Parameter(param)
+	}
+	return params
+}
+
+type Issue struct {
+	Type string `json:"issueType" validate:"required"`
+}
+
+type TestItemResourceNew struct {
+	TestItemResource
+	StartTime time.Time `json:"startTime"`
+	EndTime   time.Time `json:"endTime"`
+}
+
+type TestItemResourceOld struct {
+	TestItemResource
+	StartTime int64 `json:"startTime"`
+	EndTime   int64 `json:"endTime"`
+}
+
+type TestItemResource struct {
+	UUID        string           `json:"uuid"`
+	Name        string           `json:"name"`
+	Type        model.ItemType   `json:"type"`
+	Status      model.ItemStatus `json:"status"`
+	CodeRef     string           `json:"codeRef"`
+	TestCaseId  string           `json:"testCaseId"`
+	Description string           `json:"description"`
+	Parameters  Parameters       `json:"parameters"`
+	Attributes  Attributes       `json:"attributes"`
+	Issue       string           `json:"issue"`
+	UndefinedTestItemFields
+}
+
+type UndefinedTestItemFields struct {
+	ID               int64       `json:"id"`
+	LaunchStatus     string      `json:"launchStatus"`
+	Parent           int64       `json:"parent"`
+	PathNames        interface{} `json:"pathNames"`
+	HasStats         bool        `json:"hasStats"`
+	HasChildren      bool        `json:"hasChildren"`
+	TestCaseHash     int         `json:"testCaseHash"`
+	LaunchId         int64       `json:"launchId"`
+	UniqueId         string      `json:"uniqueId"`
+	PatternTemplates []string    `json:"patternTemplates"`
+	Statistics       interface{} `json:"statistics"`
+	Path             string      `json:"path"`
 }
--- internal/model/item.go
+++ internal/model/item.go
@@ -1,6 +1,59 @@
 package model
 
-type Item struct{}
+import "time"
+
+const (
+	ItemTypeSuite        ItemType = "SUITE"
+	ItemTypeStory        ItemType = "STORY"
+	ItemTypeTest         ItemType = "TEST"
+	ItemTypeScenario     ItemType = "SCENARIO"
+	ItemTypeStep         ItemType = "STEP"
+	ItemTypeBeforeClass  ItemType = "BEFORE_CLASS"
+	ItemTypeBeforeGroups ItemType = "BEFORE_GROUPS"
+	ItemTypeBeforeMethod ItemType = "BEFORE_METHOD"
+	ItemTypeBeforeSuite  ItemType = "BEFORE_SUITE"
+	ItemTypeBeforeTest   ItemType = "BEFORE_TEST"
+	ItemTypeAfterClass   ItemType = "AFTER_CLASS"
+	ItemTypeAfterGroups  ItemType = "AFTER_GROUPS"
+	ItemTypeAfterMethod  ItemType = "AFTER_METHOD"
+	ItemTypeAfterSuite   ItemType = "AFTER_SUITE"
+	ItemTypeAfterTest    ItemType = "AFTER_TEST"
+)
+
+const (
+	ItemStatusPassed      ItemStatus = "PASSED"
+	ItemStatusFailed      ItemStatus = "FAILED"
+	ItemStatusSkipped     ItemStatus = "SKIPPED"
+	ItemStatusStopped     ItemStatus = "STOPPED"
+	ItemStatusInterrupted ItemStatus = "INTERRUPTED"
+	ItemStatusCancelled   ItemStatus = "CANCELLED"
+	ItemStatusInfo        ItemStatus = "INFO"
+	ItemStatusWarn        ItemStatus = "WARN"
+)
+
+type Item struct {
+	UUID        string     `json:"uuid"`
+	LaunchUUID  string     `json:"launch_uuid"`
+	Name        string     `json:"name"`
+	Description string     `json:"description"`
+	Type        ItemType   `json:"type"`
+	Status      ItemStatus `json:"status"`
+	StartTime   time.Time  `json:"start_time"`
+	EndTime     time.Time  `json:"end_time"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	Attributes  Attributes `json:"attributes"`
+	Parameters  Parameters `json:"parameters"`
+	CodeRef     string     `json:"code_ref"`
+	TestCaseId  string     `json:"test_case_id"`
+	ParentUUID  string     `json:"parent_uuid"`
+	IsRetry     bool       `json:"retry"`
+	RetryOf     string     `json:"retry_of"`
+	Issue       Issue      `json:"issue"`
+}
+
+type ItemType string
+
+type ItemStatus string
 
 type Attribute struct {
 	Key      string `json:"key"`
@@ -9,3 +62,14 @@ type Attribute struct {
 }
 
 type Attributes []Attribute
+
+type Parameter struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type Parameters []Parameter
+
+type Issue struct {
+	Type string `json:"type"`
+}
