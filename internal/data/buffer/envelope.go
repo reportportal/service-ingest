f0bbca2d7f3a042487531f427cc01d79120daf3b
Reingold Shekhtel <raikbitters@gmail.com>
2025-12-18 18:22:46 +0100

    Refactor EventEnvelope structure to improve lease and tracing management

--- internal/data/buffer/envelope.go
+++ internal/data/buffer/envelope.go
@@ -6,16 +6,20 @@ import (
 )
 
 type EventEnvelope struct {
-	ID             string          `json:"id"`
-	EntityUUID     string          `json:"entity_uuid"`
-	RequestID      string          `json:"request_id"`
-	EntityType     string          `json:"entity_type"`
-	Operation      string          `json:"operation"`
-	Timestamp      time.Time       `json:"timestamp"`
-	Data           json.RawMessage `json:"data"`
-	Size           int64           `json:"size"`
-	LeaseID        string          `json:"lease_id,omitempty"`
-	LeaseExpiresAt *time.Time      `json:"lease_expires_at,omitempty"`
+	ID         string          `json:"id"`
+	EntityUUID string          `json:"entity_uuid"`
+	EntityType string          `json:"entity_type"`
+	Operation  string          `json:"operation"`
+	Timestamp  time.Time       `json:"timestamp"`
+	Data       json.RawMessage `json:"data"`
+	Size       int64           `json:"size"`
+
+	//Tracing
+	RequestID string `json:"request_id,omitempty"`
+
+	//Lease management
+	LeaseID        string     `json:"lease_id,omitempty"`
+	LeaseExpiresAt *time.Time `json:"lease_expires_at,omitempty"`
 }
 
 func (e *EventEnvelope) IsAvailable() bool {
