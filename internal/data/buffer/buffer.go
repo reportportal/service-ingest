678e18baa534071d4479b1180a03070c15f41006
Reingold Shekhtel <raikbitters@gmail.com>
2025-12-19 20:21:33 +0100

    Refactor Buffer interface to use EventEnvelope for Ack and Release methods

--- internal/data/buffer/buffer.go
+++ internal/data/buffer/buffer.go
@@ -5,8 +5,8 @@ import "context"
 type Buffer interface {
 	Put(ctx context.Context, envelope EventEnvelope) error
 	Read(ctx context.Context, limit int) ([]EventEnvelope, error)
-	Ack(ctx context.Context, ids []string) error
-	Release(ctx context.Context, ids []string) error
+	Ack(ctx context.Context, events []EventEnvelope) error
+	Release(ctx context.Context, events []EventEnvelope) error
 	Size(ctx context.Context) (count int, bytes int64, err error)
 	Close() error
 }
