9af9187d33aa59e7cfb48979c58c77ab9b1f5f2b
Reingold Shekhtel <raikbitters@gmail.com>
2025-12-22 16:17:44 +0100

    Add BatchWindow field to StorageConfig and implement BatchWindowDuration method

--- internal/config/storage.go
+++ internal/config/storage.go
@@ -8,6 +8,7 @@ type StorageConfig struct {
 	CatalogPath   string `env:"CATALOG_PATH" envDefault:"/data/catalog"`
 	BufferPath    string `env:"BUFFER_PATH" envDefault:"/data/buffer"`
 	FlushInterval string `env:"FLUSH_INTERVAL" envDefault:"30s"`
+	BatchWindow   string `env:"BATCH_WINDOW" envDefault:"60s"`
 
 	ParquetCompression  string `env:"PARQUET_COMPRESSION" envDefault:"snappy"`
 	ParquetRowGroupSize int    `env:"PARQUET_ROW_GROUP_SIZE" envDefault:"1000"`
@@ -15,4 +16,8 @@ type StorageConfig struct {
 
 func (s StorageConfig) FlushIntervalDuration() (time.Duration, error) {
 	return time.ParseDuration(s.FlushInterval)
+}
+
+func (s StorageConfig) BatchWindowDuration() (time.Duration, error) {
+	return time.ParseDuration(s.BatchWindow)
 }
