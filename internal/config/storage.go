093ed3d6a9c4517639838a2f829f4d323157fb2a
Reingold Shekhtel <raikbitters@gmail.com>
2025-12-22 14:29:03 +0100

    Refactor FlushIntervalDuration method in StorageConfig for improved readability

--- internal/config/storage.go
+++ internal/config/storage.go
@@ -8,20 +8,20 @@ type StorageConfig struct {
 	Type string `env:"STORAGE_TYPE" envDefault:"local"`
 
 	CatalogPath string `env:"CATALOG_PATH" envDefault:"/data/catalog"`
+
 	BufferPath  string `env:"BUFFER_PATH" envDefault:"/data/buffer"`
 	BufferLease string `env:"BUFFER_LEASE_DURATION" envDefault:"5m"`
 
 	FlushInterval string `env:"FLUSH_INTERVAL" envDefault:"30s"`
-	FlushMaxItems string `env:"FLUSH_MAX_ITEMS" envDefault:"1000"`
 
 	ParquetCompression  string `env:"PARQUET_COMPRESSION" envDefault:"snappy"`
 	ParquetRowGroupSize int    `env:"PARQUET_ROW_GROUP_SIZE" envDefault:"1000"`
 }
 
-func (s StorageConfig) FlushIntervalDuration() (time.Duration, error) {
-	return time.ParseDuration(s.FlushInterval)
-}
-
 func (s StorageConfig) BufferLeaseDuration() (time.Duration, error) {
 	return time.ParseDuration(s.BufferLease)
+}
+
+func (s StorageConfig) FlushIntervalDuration() (time.Duration, error) {
+	return time.ParseDuration(s.FlushInterval)
 }
