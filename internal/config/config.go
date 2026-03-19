d9d6daf5c07f95aed9ec0546aa91db5c3debcf0c
Reingold Shekhtel <raikbitters@gmail.com>
2026-03-19 13:05:53 +0100

    Fix `ServerConfig.Address` env default to use `HOST` and `PORT`

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,7 +17,7 @@ type Config struct {
 type ServerConfig struct {
 	Port     int    `env:"PORT" envDefault:"8080"`
 	Host     string `env:"HOST" envDefault:"0.0.0.0"`
-	Address  string `env:"ADDRESS,expand" envDefault:"$SCIM_HOST:$SCIM_PORT"`
+	Address  string `env:"ADDRESS,expand" envDefault:"$HOST:$PORT"`
 	BasePath string `env:"BASE_PATH" envDefault:"/api"`
 }
 
